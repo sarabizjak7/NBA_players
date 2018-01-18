cf32fd858cd5e4b036e312acf6c7671937618255
Sara Bizjak <sarabizjak97@gmail.com>
2018-01-18 23:56:15 +0100

    Working

--- tools.py
+++ tools.py
@@ -3,61 +3,59 @@ import os
 import re
 import string
 import csv
+import sys
 
-
-### http://en.hispanosnba.com/players/nba-all/a -priimk na a
-### http://en.hispanosnba.com/players/nba-all/index -prva stran
-
-### začne se v 103 vrstici
-### prettify - 450 vrstica
 
 nba_frontpage_url = 'http://en.hispanosnba.com/players/nba-all/index'
 base_url = 'http://en.hispanosnba.com'
 nba_directory = 'nba_dir'
 html_filename = 'nba_list.html'
+
 csv_filename = 'nba_list.csv'
 
 
 def make_directory(filename):
-    '''If not elready exist, it creates an empty directory for the given file'''
+    '''If not elready exist, create an empty directory for the given file'''
     directory = os.path.dirname(filename)
     if directory:
         os.makedirs(filename, exist_ok=True)
 
+
 def save_page_to_file(url, filename, forced_download = False):
     '''Saves page on given url to given file'''
     try:
-        print('Saving {}...'.format(url), end='')
-        r = requests.get(url)
+        print('Saving {}... '.format(url), end='')
+        sys.stdout.flush()
         if os.path.isfile(filename) and not forced_download:
             print('Already saved')
             return
+        r = requests.get(url)
     except requests.exceptions.ConnectionError:
         print('failed to connect to url' + url)
         return
     else:
-        make_a_directory(filename)
-        with open(filename, 'w') as f:
+        make_directory(filename)
+        with open(filename, 'w', encoding='utf8') as f:
             f.write(r.text)
             print('Saved!')
 
 
 def content_of_file(filename):
-    '''Returns string with content of given file'''
+    '''Return string with content of given file'''
     with open(filename) as f:
         content = f.read()
     return content
 
 
 def files(directory):
-    '''Returns names of all files in given directory and also a directory name'''
+    '''Return names of all files in given directory and also a directory name'''
     return [os.path.join(directory, filename) for file in os.listdir(directory)]
 
 
 def make_table(dictionaries, fields, filename):
-    '''From a dictionary list makes CSV file'''
+    '''From a dictionary list make CSV file'''
     make_directory(filename)
-    with open(filename, 'w') as csv_file:
+    with open(filename, 'w', encoding='utf8') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=fields)
         writer.writeheader()
         for dictionary in dictionaries:
